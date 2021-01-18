b02d9c3fcc0baa31b62eeadd3f71e2f83c5be210
keira <keirakaitlynn@gmail.com>
2021-01-18 15:44:19 -0800

    Fixed main-content section.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,65 +2,124 @@ import React from "react";
 
 function MainContent() {
   return (
-    <main className="main">
-      <div className="main-container" id="tour">
-        <div className="main-content">
-          <div className="main-header-container">
-            <a className="main-header">Tour</a>
+    <main>
+      <section className="tour">
+        <div className="tour-content">
+          <div className="tour-header">
+            <p>Tour</p>
           </div>
-
-          <ul className="event-container">
-            <a>
-              <li>14th February 2021</li>
-            </a>
-            <a>
-              <li>Wardrobe Presented by Crash Records [Matinee]</li>
-            </a>
-            <a>
-              <li>Leeds, United Kingdom</li>
-            </a>
-            <a>
-              <div className="event-ticket-button">
-                <p>Get Tickets</p>
-              </div>
-            </a>
-          </ul>
-
-          <ul className="event-container">
-            <a>
-              <li>15th February 2021</li>
-            </a>
-            <a>
-              <li>Pryzm Presented by Banquet Records</li>
-            </a>
-            <a>
-              <li>Kingston, United Kingdom</li>
-            </a>
-            <a>
-              <div className="event-ticket-button">
-                <p>Get Tickets</p>
-              </div>
-            </a>
-          </ul>
-
-          <ul className="event-container">
-            <a>
-              <li>16th February 2021</li>
-            </a>
-            <a>
-              <li>Chalk Presented By Resident</li>
-            </a>
-            <a>
-              <li>Brighton, United Kingdom</li>
-            </a>
-            <a>
-              <div className="event-ticket-button">
-                <p>Get Tickets</p>
-              </div>
-            </a>
-          </ul>
+          <div className="event">
+            <div className="event-container">
+              <ul className="event-left">
+                <a>
+                  <li>Location</li>
+                </a>
+                <a>
+                  <li>Venue</li>
+                </a>
+                <a>
+                  <li>Date</li>
+                </a>
+              </ul>
+              <ul className="event-right">
+                <a>
+                  <li>
+                    <p>Get Tickets</p>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-container">
+              <ul className="event-left">
+                <a>
+                  <li>Location</li>
+                </a>
+                <a>
+                  <li>Venue</li>
+                </a>
+                <a>
+                  <li>Date</li>
+                </a>
+              </ul>
+              <ul className="event-right">
+                <a>
+                  <li>
+                    <p>Get Tickets</p>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-container">
+              <ul className="event-left">
+                <a>
+                  <li>Location</li>
+                </a>
+                <a>
+                  <li>Venue</li>
+                </a>
+                <a>
+                  <li>Date</li>
+                </a>
+              </ul>
+              <ul className="event-right">
+                <a>
+                  <li>
+                    <p>Get Tickets</p>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-container">
+              <ul className="event-left">
+                <a>
+                  <li>Location</li>
+                </a>
+                <a>
+                  <li>Venue</li>
+                </a>
+                <a>
+                  <li>Date</li>
+                </a>
+              </ul>
+              <ul className="event-right">
+                <a>
+                  <li>
+                    <p>Get Tickets</p>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="event">
+            <div className="event-container">
+              <ul className="event-left">
+                <a>
+                  <li>Location</li>
+                </a>
+                <a>
+                  <li>Venue</li>
+                </a>
+                <a>
+                  <li>Date</li>
+                </a>
+              </ul>
+              <ul className="event-right">
+                <a>
+                  <li>
+                    <p>Get Tickets</p>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
