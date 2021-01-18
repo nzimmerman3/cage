3813dd00576bf8e7c488c702580d2f4bca8d1373
keira <keirakaitlynn@gmail.com>
2021-01-17 18:14:06 -0800

    Added more stuff to main-content.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,8 +6,10 @@ function MainContent() {
         <div className="main-container">
           <div className="main-content">
 
-            <div className="main-header">
-              <a>Tour</a>
+            <div className="main-header-container">
+              <a className="main-header">
+                Tour
+              </a>
             </div>
 
             <ul className="event-container">
@@ -24,7 +26,62 @@ function MainContent() {
                   <p>Get Tickets</p>
                 </a>
             </ul>
-
+            <ul className="event-container">
+                <li className="event-date">
+                  15th February 2021
+                </li>
+                <li className="event-venue">
+                  Pryzm Presented by Banquet Records
+                </li>
+                <li className="event-location">
+                  Kingston, United Kingdom
+                </li>
+                <a className="event-ticket-button">
+                  <p>Get Tickets</p>
+                </a>
+            </ul>
+            <ul className="event-container">
+                <li className="event-date">
+                  14th February 2021
+                </li>
+                <li className="event-venue">
+                  Wardrobe Presented by Crash Records [Matinee]
+                </li>
+                <li className="event-location">
+                  Leeds, United Kingdom
+                </li>
+                <a className="event-ticket-button">
+                  <p>Get Tickets</p>
+                </a>
+            </ul>
+            <ul className="event-container">
+                <li className="event-date">
+                  15th February 2021
+                </li>
+                <li className="event-venue">
+                  Pryzm Presented by Banquet Records
+                </li>
+                <li className="event-location">
+                  Kingston, United Kingdom
+                </li>
+                <a className="event-ticket-button">
+                  <p>Get Tickets</p>
+                </a>
+            </ul>
+            <ul className="event-container">
+                <li className="event-date">
+                  14th February 2021
+                </li>
+                <li className="event-venue">
+                  Wardrobe Presented by Crash Records [Matinee]
+                </li>
+                <li className="event-location">
+                  Leeds, United Kingdom
+                </li>
+                <a className="event-ticket-button">
+                  <p>Get Tickets</p>
+                </a>
+            </ul>
             <ul className="event-container">
                 <li className="event-date">
                   15th February 2021
