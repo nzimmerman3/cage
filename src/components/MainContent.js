97c92e1413b87f3ebd960cd044dbb2af1ed3d2ca
keira <keirakaitlynn@gmail.com>
2021-01-17 20:47:26 -0800

    Added more stuff to main-content.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,93 +1,64 @@
-import React from "react"
+import React from "react";
 
 function MainContent() {
-    return (
-      <main className="main">
-
-        <div className="main-container" id="tour">
-          <div className="main-content">
-
-            <div className="main-header-container">
-              <a className="main-header">
-                Tour
-              </a>
-            </div>
-
-            <ul className="event-container">
-                <li className="event-date">
-                  14th February 2021
-                </li>
-                <li className="event-venue">
-                  Wardrobe Presented by Crash Records [Matinee]
-                </li>
-                <li className="event-location">
-                  Leeds, United Kingdom
-                </li>
-                <a className="event-ticket-button">
-                  <p>Get Tickets</p>
-                </a>
-            </ul>
-            <ul className="event-container">
-                <li className="event-date">
-                  15th February 2021
-                </li>
-                <li className="event-venue">
-                  Pryzm Presented by Banquet Records
-                </li>
-                <li className="event-location">
-                  Kingston, United Kingdom
-                </li>
-                <a className="event-ticket-button">
-                  <p>Get Tickets</p>
-                </a>
-            </ul>
-            <ul className="event-container">
-                <li className="event-date">
-                  14th February 2021
-                </li>
-                <li className="event-venue">
-                  Wardrobe Presented by Crash Records [Matinee]
-                </li>
-                <li className="event-location">
-                  Leeds, United Kingdom
-                </li>
-                <a className="event-ticket-button">
-                  <p>Get Tickets</p>
-                </a>
-            </ul>
-            <ul className="event-container">
-                <li className="event-date">
-                  15th February 2021
-                </li>
-                <li className="event-venue">
-                  Pryzm Presented by Banquet Records
-                </li>
-                <li className="event-location">
-                  Kingston, United Kingdom
-                </li>
-                <a className="event-ticket-button">
-                  <p>Get Tickets</p>
-                </a>
-            </ul>
-            <ul className="event-container">
-                <li className="event-date">
-                  14th February 2021
-                </li>
-                <li className="event-venue">
-                  Wardrobe Presented by Crash Records [Matinee]
-                </li>
-                <li className="event-location">
-                  Leeds, United Kingdom
-                </li>
-                <a className="event-ticket-button">
-                  <p>Get Tickets</p>
-                </a>
-            </ul>
-
+  return (
+    <main className="main">
+      <div className="main-container" id="tour">
+        <div className="main-content">
+          <div className="main-header-container">
+            <a className="main-header">Tour</a>
           </div>
-        </div>
-      </main>
-    )
-  }
 
-  export default MainContent;
+          <ul className="event-container">
+            <li className="event-date">14th February 2021</li>
+            <li className="event-venue">
+              Wardrobe Presented by Crash Records [Matinee]
+            </li>
+            <li className="event-location">Leeds, United Kingdom</li>
+            <a className="event-ticket-button">
+              <p>Get Tickets</p>
+            </a>
+          </ul>
+          <ul className="event-container">
+            <li className="event-date">15th February 2021</li>
+            <li className="event-venue">Pryzm Presented by Banquet Records</li>
+            <li className="event-location">Kingston, United Kingdom</li>
+            <a className="event-ticket-button">
+              <p>Get Tickets</p>
+            </a>
+          </ul>
+          <ul className="event-container">
+            <li className="event-date">14th February 2021</li>
+            <li className="event-venue">
+              Wardrobe Presented by Crash Records [Matinee]
+            </li>
+            <li className="event-location">Leeds, United Kingdom</li>
+            <a className="event-ticket-button">
+              <p>Get Tickets</p>
+            </a>
+          </ul>
+          <ul className="event-container">
+            <li className="event-date">15th February 2021</li>
+            <li className="event-venue">Pryzm Presented by Banquet Records</li>
+            <li className="event-location">Kingston, United Kingdom</li>
+            <a className="event-ticket-button">
+              <p>Get Tickets</p>
+            </a>
+          </ul>
+          <ul className="event-container">
+            <li className="event-date">14th February 2021</li>
+            <li className="event-venue">
+              Wardrobe Presented by Crash Records [Matinee]
+            </li>
+            <li className="event-location">Leeds, United Kingdom</li>
+            <a className="event-ticket-button">
+              <p>Get Tickets</p>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
