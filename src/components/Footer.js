6455ef91cf1bd066cca8f1242f01d5b383062841
keira <keirakaitlynn@gmail.com>
2021-01-20 12:12:42 -0800

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -63,8 +63,14 @@ function Footer() {
             </a>
           </ul>
           <p>
-            &copy; 2021 SONY MUSIC ENTERTAINMENT UK LTD. | PRIVACY &amp; COOKIE
-            POLICY
+            &copy; 2021{" "}
+            <a href="https://www.sonymusic.co.uk/">
+              SONY MUSIC ENTERTAINMENT UK LTD.
+            </a>{" "}
+            |{" "}
+            <a href="https://tools.sonymusiccreative.com/privacy/PCP_UK.pdf">
+              PRIVACY &amp; COOKIE POLICY
+            </a>
           </p>
         </div>
       </div>
