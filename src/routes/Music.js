faadd677609ffa0675a7581914a97a13b3f55bed
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2021-02-24 11:31:53 -0800

    Added spotify player

--- src/routes/Music.js
+++ src/routes/Music.js
@@ -4,9 +4,20 @@ import AlbumTMIP from "../images/tell-me-im-pretty.jpg";
 import AlbumM from "../images/melophobia.jpg";
 import SpotifyLogo from "../images/spotify-logo-white.png";
 
+import SpotifyPlayer from 'react-spotify-player'
+
 export default function Music() {
+  const size = {
+    width: '100%',
+    height: 300,
+  };
+  const view = 'list'; // or 'coverart'
+  const theme = 'black'; // or 'white'
+
+
   return (
     <main>
+
       <section className="music">
 
         <div className="music-header">
@@ -14,23 +25,29 @@ export default function Music() {
         </div>
 
         <div className="music-content">
+          <SpotifyPlayer
+            uri="spotify:album:4EK8gtQfdVsmDTji7gBFlz"
+            size={size}
+            view={view}
+            theme={theme}
+          />
 
           <div className="song">
             <div className="song-container">
               <ul className="song-left">
                 <a>
                   <li><h1>06: Halo</h1>
-                  <h2>Melophobia</h2>
-                  <div className="follow-button"><p>Follow</p></div></li>
+                    <h2>Melophobia</h2>
+                    <div className="follow-button"><p>Follow</p></div></li>
                 </a>
                 <a>
-                  <li><img src={AlbumM} alt="album"/></li>
+                  <li><img src={AlbumM} alt="album" /></li>
                 </a>
               </ul>
               <ul className="song-right">
                 <a>
                   <li>
-                    <p><img src={SpotifyLogo}/></p>
+                    <p><img src={SpotifyLogo} /></p>
                   </li>
                 </a>
               </ul>
@@ -38,7 +55,7 @@ export default function Music() {
           </div>
 
         </div>
-        
+
       </section>
     </main>
   );
