6551891a6ff076e31ba557d912269167c53b51f7
keira <keirakaitlynn@gmail.com>
2021-03-30 21:59:18 -0700

    Reformatted tour section and applied bootstrap styles to tour-header & tour-content.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,199 +5,47 @@ import BlackTape from "../images/black-tape.png";
 
 function MainContent() {
   return (
-    <main>
-      <Header />
-      <div className="tour" id="tour">
-        <div class="col-lg-12" className="tour-header">
-          <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="400px"/>
-          <p class="text-center">Tour</p>
-        </div>
-        {/* <div className="tour-header"><p>Tour</p></div> */}
-        <div className="tour-content" >
-          <div className="event" class="row event-border">
-            <div className="event-left">
-              <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+    <div className="main">
+      <main>
+
+        <Header />
+
+        <div className="tour" id="tour">
+          <div class="col-lg-12" className="tour-header">
+            <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="400px"/>
+            <p class="text-center">Tour</p>
+          </div>
+          <div class="tour-content col-lg-12">
+            {/* ------ EVENT 1 ------ */}
+            <div class="event row no-gutter ">
+              <div class="col-lg-10 no-gutter align-items-center">
+
+                <div class="col-lg-3 ">
                   <div>14th February 2021</div>
                 </div>
-                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <div class="col-lg-5">
                   <div>Wardrobe Presented by Crash Records</div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div class="col-lg-4">
                   <div>Leeds, United Kingdom</div>
                 </div>
+
               </div>
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-              <button type="button" class="btn btn-default btn-xl">Get Tickets</button>
+              <div class="col-lg-2 no-gutter button-padding">
+                
+                <div class="col-lg-12">
+                  <button type="button" class="btn btn-default btn-xl">Get Tickets</button>
+                </div>
+
+              </div>
             </div>
+            {/* ------ EVENT 1 ------ */}
           </div>
         </div>
-      </div>
-      {/* <section className="tour" id="tour">
-        <div className="tour-header">
-          <img src={BlackTape} alt="" />
-          <p>Tour</p>
-        </div>
-        <div className="tour-content">
-          <div className="event">
-            <div className="event-container">
-              <ul className="event-left">
-                <a>
-                  <li>Location</li>
-                </a>
-                <a>
-                  <li>Venue</li>
-                </a>
-                <a>
-                  <li>Date</li>
-                </a>
-              </ul>
-              <ul className="event-right">
-                <a>
-                  <li>
-                    <p>Get Tickets</p>
-                  </li>
-                </a>
-              </ul>
-            </div>
-          </div>
-          <div className="tour-content">
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="event">
-              <div className="event-container">
-                <ul className="event-left">
-                  <a>
-                    <li>Location</li>
-                  </a>
-                  <a>
-                    <li>Venue</li>
-                  </a>
-                  <a>
-                    <li>Date</li>
-                  </a>
-                </ul>
-                <ul className="event-right">
-                  <a>
-                    <li>
-                      <p>Get Tickets</p>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
-      <Newsletter />
-    </main>
+
+        <Newsletter />
+      </main>
+    </div>
   );
 }
 
