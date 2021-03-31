f32273b0e1205b161b199c27100c83adcf2b34bf
keira <keirakaitlynn@gmail.com>
2021-03-31 11:17:41 -0700

    Centered main background, tour-section background, and footer background.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -11,29 +11,29 @@ function MainContent() {
         <Header />
 
         <div className="tour" id="tour">
-          <div class="col-lg-12" className="tour-header">
-            <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="400px"/>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" className="tour-header">
+            <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="500px"/>
             <p class="text-center">Tour</p>
           </div>
           <div class="tour-content col-lg-12">
             {/* ------ EVENT 1 ------ */}
             <div class="event row no-gutter ">
-              <div class="col-lg-10 no-gutter align-items-center">
+              <div class="col-lg-7 no-gutter align-items-center">
 
-                <div class="col-lg-3 ">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                   <div>14th February 2021</div>
                 </div>
-                <div class="col-lg-5">
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
                   <div>Wardrobe Presented by Crash Records</div>
-                </div>
-                <div class="col-lg-4">
-                  <div>Leeds, United Kingdom</div>
                 </div>
 
               </div>
-              <div class="col-lg-2 no-gutter button-padding">
+              <div class="col-lg-5 no-gutter">
                 
-                <div class="col-lg-12">
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                  <div>Leeds, United Kingdom</div>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
                   <button type="button" class="btn btn-default btn-xl">Get Tickets</button>
                 </div>
 
