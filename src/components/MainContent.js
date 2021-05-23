6f6b097db286521c4be7f404cfafa45cde8101d2
keira <keirakaitlynn@gmail.com>
2021-05-23 15:55:00 -0700

    Deleted extra code in MainContent.js & added more events to EventList.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -24,28 +24,6 @@ function MainContent() {
           <div class="tour-content col-md-10 col-md-offset-1 container">
             {/* ------ EVENT 1 ------ */}
             <EventsList></EventsList>
-            {/* <div id="outerDiv" class="event row no-gutter center-block vert-center">
-              <div class="col-lg-7 no-gutter vert-center">
-
-                <div class="col-lg-4 ">
-                  <div>14th February 2021</div>
-                </div>
-                <div class="col-lg-8">
-                  <div>Wardrobe Presented by Crash Records</div>
-                </div>
-
-              </div>
-              <div class="col-lg-5 no-gutter vert-center">
-                
-                <div class="col-lg-7 gutter">
-                  <div>Leeds, United Kingdom</div>
-                </div>
-                <div class="col-lg-5 gutter">
-                    <button type="button" class="btn btn-default btn-xl">Get Tickets</button>
-                </div>
-
-              </div>
-            </div> */}
             {/* ------ EVENT 1 ------ */}
           </div>
         </div>
--- src/routes/EventsList.js
+++ src/routes/EventsList.js
@@ -3,18 +3,74 @@ import Event from './Event'
 function EventsList() {
     const events = [
         {
-          date: '02-14-2021',
-          venue: 'Wardrobe Presented by Crash Records',
-          city: 'Leeds',
-          country: 'United Kingdom',
-          onSale: 'Y'
+            date: '02-14-2021',
+            venue: 'Wardrobe Presented by Crash Records',
+            city: 'Leeds',
+            country: 'United Kingdom',
+            onSale: 'Y'
         },
         {
-          date: '02-09-2021',
-          venue: 'Pryzm Presented by Banquet Records',
-          city: 'Kingston',
-          country: 'United Kingdom',
-          onSale: 'N'
+            date: '02-09-2021',
+            venue: 'Pryzm Presented by Banquet Records',
+            city: 'Kingston',
+            country: 'United Kingdom',
+            onSale: 'N'
+        },
+        {
+            date: '02-14-2021',
+            venue: 'Wardrobe Presented by Crash Records',
+            city: 'Leeds',
+            country: 'United Kingdom',
+            onSale: 'Y'
+        },
+        {
+            date: '02-09-2021',
+            venue: 'Pryzm Presented by Banquet Records',
+            city: 'Kingston',
+            country: 'United Kingdom',
+            onSale: 'N'
+        },
+        {
+            date: '02-14-2021',
+            venue: 'Wardrobe Presented by Crash Records',
+            city: 'Leeds',
+            country: 'United Kingdom',
+            onSale: 'Y'
+        },
+        {
+            date: '02-09-2021',
+            venue: 'Pryzm Presented by Banquet Records',
+            city: 'Kingston',
+            country: 'United Kingdom',
+            onSale: 'N'
+        },
+        {
+            date: '02-09-2021',
+            venue: 'Pryzm Presented by Banquet Records',
+            city: 'Kingston',
+            country: 'United Kingdom',
+            onSale: 'N'
+        },
+        {
+            date: '02-14-2021',
+            venue: 'Wardrobe Presented by Crash Records',
+            city: 'Leeds',
+            country: 'United Kingdom',
+            onSale: 'Y'
+        },
+        {
+            date: '02-09-2021',
+            venue: 'Pryzm Presented by Banquet Records',
+            city: 'Kingston',
+            country: 'United Kingdom',
+            onSale: 'N'
+        },
+        {
+            date: '02-14-2021',
+            venue: 'Wardrobe Presented by Crash Records',
+            city: 'Leeds',
+            country: 'United Kingdom',
+            onSale: 'Y'
         }
     ]
     const eventsList = events.map(event => <Event event={event}/>)
