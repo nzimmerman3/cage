29d04adc5fc849c721b59d6063c9cc3aa2711517
keira <keirakaitlynn@gmail.com>
2021-05-23 22:32:11 -0700

    Refactored MainContent.js & Tour.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "./Header"
 import Newsletter from "./Newsletter"
-import BlackTape from "../images/black-tape.png";
-import ChromeInitials from "../images/chrome-initials.gif";
-import EventsList from "../routes/EventsList";
+import Tour from "../routes/Tour"
+import ChromeInitials from "../images/chrome-initials.gif"
+import Footer from "./Footer"
+
 
 function MainContent() {
   return (
@@ -12,23 +13,13 @@ function MainContent() {
 
         <Header />
 
-        {/* <img src={ChromeInitials} class="img-responsive center-block d-block mx-auto" alt="" width="500px"/> */}
+        <img src={ChromeInitials} class="img-responsive center-block d-block mx-auto" alt="" width="500px"/>
 
-        <div className="tour" id="tour">
-          
-          <div class="" className="tour-header">
-            <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="500px"/>
-            <p class="text-center">Tour</p>
-          </div>
-
-          <div class="tour-content col-md-10 col-md-offset-1 container">
-            {/* ------ EVENT 1 ------ */}
-            <EventsList></EventsList>
-            {/* ------ EVENT 1 ------ */}
-          </div>
+        <div className="main-content">
+          <Tour />
+          <Newsletter />
         </div>
 
-        <Newsletter />
       </main>
     </div>
   );
--- src/routes/Tour.js
+++ src/routes/Tour.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from "../components/SubHeader"
+import BlackTape from "../images/black-tape.png";
+import EventsList from "../routes/EventsList";
 
 export default function Tour() {
   return (
     <div>
-      <Header />
-      tour
+      <div className="tour" id="tour">
+          <div class="" className="tour-header">
+            <img src={BlackTape} class="img-responsive center-block d-block mx-auto" alt="" width="500px"/>
+            <p class="text-center">Tour</p>
+          </div>
+          <div class="tour-content col-md-10 col-md-offset-1 container">
+            {/* ------ EVENT 1 ------ */}
+            <EventsList></EventsList>
+            {/* ------ EVENT 1 ------ */}
+          </div>
+        </div>
     </div>
   )
 }
