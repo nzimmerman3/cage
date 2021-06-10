8c28a298036c9af5b76517e103744415bbb2435a
keira <keirakaitlynn@gmail.com>
2021-06-09 22:01:27 -0700

    Added Products row in Shop.js.

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -3,8 +3,14 @@ import Header from "../components/SubHeader"
 import Tour from './Tour'
 import Photo from "../images/display-photos/neil-krug-7-cropped.jpg"
 import ShopHeader from './ShopHeader'
+import Product1 from '../images/products/yellow-shirt.png'
+import Product2 from '../images/products/pink-tote.png'
+
 
 export default function Shop() {
+
+  const products = [Product1, Product2]
+
   return (
     <div>
         <Header />
@@ -20,6 +26,36 @@ export default function Shop() {
 
         <div className="shop-background">
             {/* Content */}
+            <div id="outerDiv" class="products row no-gutter center-block vert-center text-center">
+
+              <div class="col-lg-4">
+                <div class="col-lg-6">
+                  <div>Product1</div>
+                </div>
+                <div class="col-lg-6">
+                  <div>Product2</div>
+                </div>
+              </div>
+
+              <div class="col-lg-4">
+                <div class="col-lg-6">
+                  <div>Product3</div>
+                </div>
+                <div class="col-lg-6">
+                  <div>Product4</div>
+                </div>
+              </div>
+
+              <div class="col-lg-4">
+                <div class="col-lg-6">
+                  <div>Product5</div>
+                </div>
+                <div class="col-lg-6">
+                  <div>Product6</div>
+                </div>
+              </div>
+
+            </div>
         </div>
     </div>
   )
