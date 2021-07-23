be529a0d3524f7655e92855ee9d2447ec8624b71
keira <keirakaitlynn@gmail.com>
2021-07-23 12:32:29 -0700

    Added view product page.

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,71 +2,104 @@ import React from 'react'
 import Product1 from '../images/products/yellow-shirt.png'
 import Product2 from '../images/products/pink-tote.png'
 import Product3 from '../images/products/black-bandana.png'
+import Product4 from '../images/products/social-cues-vinyl.png'
 
 const products = [
   {
     "image": Product1,
     "name": "T-Shirt",
-    "price": "$40.00"
+    "price": "$40.00",
     //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Custom dip-dye t-shirt featuring Cage the Elephant logo on front left chest and 2019 tour dates on the back."
   },
   {
     "image": Product2,
     "name": "Tote Bag",
-    "price": "$20.00"
+    "price": "$20.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Light pink tote bag featuring melted suit portrait design on one side."
   },
   {
     "image": Product3,
     "name": "Bandana",
-    "price": "$16.00"
+    "price": "$16.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Black bandana featuring artwork from the Tell Me I'm Pretty album."
+  },
+  {
+    "image": Product4,
+    "name": "Social Cues",
+    "price": "$40.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "[TrackList]"
   },
   {
     "image": Product1,
     "name": "T-Shirt",
-    "price": "$40.00"
+    "price": "$40.00",
     //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Custom dip-dye t-shirt featuring Cage the Elephant logo on front left chest and 2019 tour dates on the back."
   },
   {
     "image": Product2,
     "name": "Tote Bag",
-    "price": "$20.00"
+    "price": "$20.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Light pink tote bag featuring melted suit portrait design on one side."
   },
   {
     "image": Product3,
     "name": "Bandana",
-    "price": "$16.00"
+    "price": "$16.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Black bandana featuring artwork from the Tell Me I'm Pretty album."
+  },
+  {
+    "image": Product4,
+    "name": "Social Cues",
+    "price": "$40.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "[TrackList]"
   },
   {
     "image": Product1,
     "name": "T-Shirt",
-    "price": "$40.00"
+    "price": "$40.00",
     //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Custom dip-dye t-shirt featuring Cage the Elephant logo on front left chest and 2019 tour dates on the back."
   },
   {
     "image": Product2,
     "name": "Tote Bag",
-    "price": "$20.00"
-  },
-  {
-    "image": Product3,
-    "name": "Bandana",
-    "price": "$16.00"
-  },
-  {
-    "image": Product1,
-    "name": "T-Shirt",
-    "price": "$40.00"
+    "price": "$20.00",
     //TODO add category (tour merch, music, apparel, etc.)
-  },
-  {
-    "image": Product2,
-    "name": "Tote Bag",
-    "price": "$20.00"
+    //TODO add description
+    "desc": "Light pink tote bag featuring melted suit portrait design on one side."
   },
   {
     "image": Product3,
     "name": "Bandana",
-    "price": "$16.00"
+    "price": "$16.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "Black bandana featuring artwork from the Tell Me I'm Pretty album."
+  },
+  {
+    "image": Product4,
+    "name": "Social Cues",
+    "price": "$40.00",
+    //TODO add category (tour merch, music, apparel, etc.)
+    //TODO add description
+    "desc": "[TrackList]"
   }
 ]
 
@@ -74,16 +107,17 @@ function Product(props) {
   // const product = products[props.id]
   return (
     <div>
-        {products.map((product, id) => (
 
+      <div>
+        {products.map((product, id) => (
           <div className="col-lg-2 product-container" key={id}>
             <img src={product.image}/>
             <h3>{product.name}</h3>
             <h4>{product.price}</h4>
-            {/* <button>Add to Cart</button> */}
           </div>
-
         ))}
+      </div>
+
     </div>
 
     
--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -5,7 +5,8 @@ import Photo from "../images/display-photos/neil-krug-7-cropped.jpg"
 import ShopHeader from './ShopHeader'
 import Product from '../components/Product'
 import Signature from '../images/signature-white.png'
-
+import Product1 from '../images/products/yellow-shirt.png'
+import ExitBtn from '../images/products/exit-btn.png'
 
 export default function Shop() {
 
@@ -24,12 +25,40 @@ export default function Shop() {
       <ShopHeader />
 
       <div className="shop-background">
-        {/* Content */}
-        <div id="outerDiv" class="products row no-gutter center-block vert-center text-center">
-
+        
+        {/*===== ProductsList =====*/}
+        {/* <div id="outerDiv" class="products row no-gutter center-block vert-center">
           <Product></Product>
+        </div> */}
+        {/*===== ProductsList =====*/}
 
+        {/*===== ProductView =====*/}
+        <div className="product-view row no-gutter center-block vert-center">
+          <div id="center-hv" className="product-view-img-container col-lg-6">
+            <div className="">
+              <img src={Product1}/>
+              <div id="center-hv" className="product-view-slider">...</div>
+            </div>
+          </div>
+          <div className="product-view-right-container col-lg-6 ">
+            <div id="outerDiv" className="col-lg-11 product-view-info-container">
+              <div className="product-view-txt-container">
+                <h2 class="row">T-Shirt</h2>
+                <h3 class="row">$40.00</h3>
+                <p class="row">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+                <div id="center-hv" className="row product-view-add-btn-container">
+                  <div>Add to Cart</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-1 product-view-exit-btn-container">
+              <div className="exit-btn">
+                <img src={ExitBtn}/>
+              </div>
+            </div>
+          </div>
         </div>
+        {/*===== ProductView =====*/}
 
       </div>
     </div>
